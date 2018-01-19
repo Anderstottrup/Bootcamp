e32455eaa63e4fdc138fdc35c553aa0abc60cb2a
Anders Tøttrup <5123677@rts-365.dk>
2018-01-19 13:23:51 +0100

    betingelser

---
+++ betingelser.js
@@ -0,0 +1,24 @@
+// let boolean = true;
+
+// if (boolean == true) {
+//     "Hej verden"
+//     console.log("Hej verden");
+// }
+
+// let tal = 3;
+// console.log(tal);
+// if (tal > 5) {
+//     console.log("højere end 5");
+// } else (tal < 5)
+// console.log("mindre end 5");
+
+// let tal = 6;
+// console.log((tal < 3 ? "mindre end 3" : "højere end 3"));
+
+// let monster = "hvid";
+// if (monster == "hvid") {
+//     console.log("yes");
+// } else {
+//     console.log("forkert");
+// }
+
