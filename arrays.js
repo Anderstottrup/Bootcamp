11737b73fe7867bbd91e007ae331df530d621e19
Anders Tøttrup <5123677@rts-365.dk>
2018-01-19 11:25:04 +0100

    Variabler

---
+++ arrays.js
@@ -0,0 +1,23 @@
+// let frugter = ["Pære", "Æble", "Banan"];
+// console.log(frugter);
+
+// let frugter = ["Pære", "Æble", "Banan"];
+// let last = frugter[1];
+// console.log(last);
+
+// let frugter = ["Pære", "Æble", "Banan"];
+// frugter[0] = "Fersken";
+// console.log(frugter);
+
+// let frugter = ["Pære", "Æble", "Banan"];
+// let all = frugter.length;
+// console.log(all);
+
+// let frugter = ["Pære", "Æble", "Banan"];
+// frugter[frugter.length] = "Appelsin";
+// console.log(frugter);
+
+// let personer = [["Fimse", "12"], ["Henrik", "82"]];
+// console.log(personer);
+
+
---
+++ variabler.js
@@ -0,0 +1,35 @@
+// let navn = ("anders");
+// console.log("navn");
+
+// let alder = ("21");
+// console.log("alder");
+
+// let byNavn = ("Roskilde");
+// console.log(byNavn);
+
+// let x = (5);
+// let y = (10);
+// console.log(x + y);
+
+// let x = (5);
+// let y = (10);
+// let v = (x + y);
+// console.log(v);
+
+// let mitFornavn = ("Anders");
+// let mitEfternavn = ("Tottrup");
+// let minAlder = (21);
+
+// console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn + ' og jeg er ' + minAlder + ' år');
+
+// let moms = (1.25);
+// let prisUdenmoms = (400);
+// let prisMedmoms = (moms * prisUdenmoms);
+// console.log(prisMedmoms);
+
+// let forste = 10;
+// let anden = 20;
+// let temp = anden;
+// anden = forste;
+// forste = temp;
+// console.log("Forste: " + forste + "\nAnden: " + anden);
