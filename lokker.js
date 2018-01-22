a57ee2df1649ce9fa9f7912075afea9a998cee07
Anders Tøttrup <5123677@rts-365.dk>
2018-01-22 13:18:46 +0100

    løkker & operatorer

---
+++ lokker.js
@@ -0,0 +1,49 @@
+let x = 0;
+// while (x <= 5) {
+//     console.log(x);
+//     x++;
+// }
+
+// do {
+//     x++;
+//     console.log(x);
+// }
+// while (x <= 5);
+
+// for (x = 0; x < 10; x++) {
+//     console.log(x);
+// }
+
+// do {
+//     x++;
+//     console.log(x);
+// }
+// while (x = 0);
+
+// for (x = 2017; x >= 1917; x--) {
+//     console.log(x);
+// }
+
+// let dage = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
+// for (let x = 0; x < dage.length; x++) {
+//     console.log(dage[x]);
+// }
+
+// let navne = ["Thorkild", "Anders", "Michael", "Hans", "Felix", "Jakob", "Rasmus", "Nico"];
+// navne.forEach(function (navn) {
+//     console.log(navn);
+// });
+
+// let navne = ["Kygo", "Alan Walker", "Martin Jensen", "Alesso"];
+// navne.forEach(function (navn) {
+//     console.log(navn);
+// });
+
+// let tal = [15, 22, 83, 4, 25, 10, 2, 89, 49, 10];
+// tal.forEach(function (random, index, arr) {
+//     if (random > tal[index + 1]) {
+//         console.log(random + " er større end " + tal[index + 1]);
+//     } else {
+//         console.log(random + " er mindre end " + tal[index + 1]);
+//     }
+// });
---
+++ operatorer.js
@@ -0,0 +1,46 @@
+// let forste = 7;
+// let anden = 12;
+// console.log(forste + anden);
+// console.log(forste - anden);
+// console.log(forste * anden);
+// console.log(forste / anden);
+// console.log(forste % anden);
+// console.log(++forste);
+// console.log(--anden);
+
+// let x = 3;
+// let y = 6;
+// x += y;
+// console.log(x);
+
+// let x = 3;
+// let y = 6;
+// x -= y;
+// console.log(x);
+
+// let x = 3;
+// let y = 6;
+// x /= y;
+// console.log(x);
+
+// let x = 3;
+// let y = 6;
+// x *= y;
+// console.log(x);
+
+// let x = 3;
+// let y = 6;
+// x %= y;
+// console.log(x);
+
+// let forste = 6;
+// let anden = 10;
+// if (forste < anden && anden > forste) {
+//     console.log("hej");
+// }
+
+// let x = 7;
+// let y = 10;
+// if (x < y || x > y) {
+//     console.log("hej");
+// }
